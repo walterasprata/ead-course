4b761973334eb4afae43a5a7bf71ad6e35a8db6b
Walter Prata <walterasprata@gmail.com>
2025-01-03 20:14:10 -0300

    feature/Cascade.All e metodo de deleção customizado

--- src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
+++ src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
@@ -1,10 +1,11 @@
 package com.ead.course.services.impl;
 
 import com.ead.course.repositories.LessonRepository;
+import com.ead.course.services.LessonService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class LessonServiceImpl {
+public class LessonServiceImpl implements LessonService {
     final LessonRepository lessonRepository;
 
     public LessonServiceImpl(LessonRepository lessonRepository) {
