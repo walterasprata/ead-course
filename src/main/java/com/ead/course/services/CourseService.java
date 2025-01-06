57e424098cfd0fe4270f050fafd4e4c21fd79403
Walter Prata <walterasprata@gmail.com>
2025-01-06 20:00:21 -0300

    Impl bean validation and controller to save course

--- src/main/java/com/ead/course/services/CourseService.java
+++ src/main/java/com/ead/course/services/CourseService.java
@@ -1,8 +1,13 @@
 package com.ead.course.services;
 
+import com.ead.course.dtos.CourseRecordDto;
 import com.ead.course.models.CourseModel;
 
 public interface CourseService {
 
     void delete(CourseModel courseModel);
+
+    CourseModel save(CourseRecordDto courseRecordDto);
+
+    boolean existsByName(String name);
 }
